89b4a6524fc11e144a1af02c6f4878cf084ffaa0
grgbibek22@gmail.com <grgbibek22@gmail.com>
2026-08-08 18:52:19 +0545

    chore: initialize backend project and add MCP configuration

--- backend/obj/Debug/net10.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net10.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc0c277b344d7ffafd84b879f8020842370f4cd3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c54967140c07ac594de72a47e6354dd06adfcc2")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
